3bbf9c116c3a9eff2d6153c17598c0500e854d62
ariegg <brain@t-h-i-n-x.net>
2016-11-07 09:29:40 +0100

    Update macrotest.py

--- macrotest.py
+++ macrotest.py
@@ -1,5 +1,24 @@
-# Test script for all kinds of macros
-# NB: Macros are called via REST by using macro(*args) or macro() if path has no values
+#   Copyright 2016 Andreas Riegg - t-h-i-n-x.net
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+#
+#   WebIOPi test script for all kinds of macros
+#
+#   NOTE: Macros that are called via REST "POST .../macros/macroname..." 
+#   are called by WebIOPi by using macro(*args) or macro() if REST path 
+#   has no agrument values at the end.
+#   Thus, using keyword arguments via macro(**kwargs) is not possible
+#   at the moment.
 
 import webiopi
 
